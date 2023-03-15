6e075bfc24973f3d9e5237209a56fb3044ae2ef7
ilyas-g <ilyas15069i@gmail.com>
2023-03-15 17:14:11 +0100

    Ajout d'un dotenv et d'une variable d'environnement pour le token

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
     // get the authentication token from local storage if it exists
-    const token = '0e7f56ac15e787c8370f2ad44ebbdefc';
+    const token = process.env.VUE_APP_TOKEN;
     // return the headers to the context so httpLink can read them
     return {
         headers: {
