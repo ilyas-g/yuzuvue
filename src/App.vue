8f5cf0f4349790f4137965a044f836ff7e12c2e4
ilyas-g <ilyas15069i@gmail.com>
2023-03-23 18:16:46 +0100

    Modification de la requête graphql et donc de l'affichage des datas

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,72 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 
+  <!-- <ul>
+    <li v-for="value in myObject" :key="value">
+      {{ value }}
+    </li>
+  </ul>
+
+  <p>{{ myObject.title }}</p> -->
+
   <p v-if="error">Something went wrong...</p>
   <p v-if="loading">Loading...</p>
-  <p v-else v-for="character in result.league.standings.nodes" :key="character.id">
+  <!-- <p v-else v-for="character in result.league.standings.nodes" :key="character.id">
     {{ character.entrant.name }}
-  </p>
+  </p> -->
+
+  <div v-else v-for="character in result.user.player.recentStandings" :key="character.id">
+    <p>{{ character.entrant.event.name }} - {{ character.entrant.event.tournament.name }} - {{ character.placement }} place</p>
+  </div>
   <div></div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import { useQuery } from '@vue/apollo-composable'
-import {STANDING_QUERY} from "./queries/queries"
+import {PLAYER_QUERY} from "./queries/queries"
 
 export default {
   name: 'App',
   setup () {
-    const { result, loading, error } = useQuery(STANDING_QUERY);
+    const { result, loading, error } = useQuery(PLAYER_QUERY);
+
+    console.log(result);
+
+    // let nodesResult = result
+
+    // console.log(result.user.player.sets.nodes);
+
+    // nodesResult.map((drink) => {
+    //   console.log(drink.event);
+    // });
+
+
+    // for (i=0; i <nodesResult.length; i++) {
+    //   if (nodesResult[i + 1] === nodesResult[i + 1]) {
+    //     console.log("okokok")
+    //   } else {
+    //     console.log('not ok!');
+    //   }
+    // }
+
+    // console.log(nodesResult)
+    // console.log('okok');
+
     return {
       result,
+      // nodesResult,
       loading, 
-      error
+      error,
+      myObject: {
+        title: 'How to do lists in Vue',
+        author: 'Jane Doe',
+        publishedAt: '2016-04-10'
+      }
     }
+  },
+  mounted() {
+    console.log(this.nodesResult)
   },
   components: {
     HelloWorld
--- src/queries/queries.js
+++ src/queries/queries.js
@@ -24,4 +24,36 @@ const STANDING_QUERY = gql`
   }
 `;
 
-export { STANDING_QUERY };
+const PLAYER_QUERY = gql`
+query YuzuPlayer {
+  user(slug: "d5ce37b2") {
+    id
+    player {
+      id
+      gamerTag
+      recentStandings(limit:3) {
+        id
+        entrant {
+          id
+          name
+          event {
+            id
+            name
+            videogame {
+              name
+              displayName
+            }
+            tournament {
+              id
+              name
+            }
+          }
+        }
+        placement
+      }
+    }
+  }
+}
+`;
+
+export { STANDING_QUERY, PLAYER_QUERY };
