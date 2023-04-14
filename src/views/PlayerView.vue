85469190a8f566cab7568427d1ba64049fb96411
ilyas-g <ilyas15069i@gmail.com>
2023-04-14 23:58:36 +0200

    Ajout d'un composant Player

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -14,12 +14,15 @@
     </div>
     <div v-else>Loading...</div>
 
+    <p>dfghhgfd</p>
+    <Player name="John" idStartGG="MKBxc2" />
   </main>
 </template>
 
 <script>
 import { useFetch } from '../utils/fetch.js'
 import Charts from '../components/Charts.vue'
+import Player from '../components/Player.vue'
 import {watch} from 'vue'
 
 export default {
@@ -37,30 +40,19 @@ export default {
   setup () {
     // const { fetchData, fetchError } = useFetch(`http://localhost:1337/api/players?populate=*&filters[name][$eq]=${this.route.params.name}`)
     const { fetchData, fetchError } = useFetch(`http://localhost:1337/api/players?populate=*&filters[name][$eq]=Ganondeurf`)
-
-    // fetchData.data.map((dat) => console.log(dat.attributes.name))
     
     watch(fetchData, () => {
       console.log(fetchData.value.meta);
     })
-//     watch(fetchData, (props) => {
-//       /* triggers on deep mutation to state */
-      
-//       // console.log(this.idStartGG);
-      
-//       // console.log(fetchData.value.data[0].attributes.idStartGG);
-//       fetchData.value.data.map((item) => {
-//       props.idStart = '300903d2'
-//       console.log(item.attributes.name);
-//     })
-// })
+
     return {
       fetchData,
       fetchError,
     }
   },
   components: {
-    Charts
+    Charts,
+    Player
   }
 }
 </script>
