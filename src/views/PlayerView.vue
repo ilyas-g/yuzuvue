85b4c07ae24d2fd71cb1b9b57430987d447c7ca7
ilyas-g <ilyas15069i@gmail.com>
2023-04-14 18:55:30 +0200

    Récupération de des datas des joueurs

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -1,24 +1,27 @@
 <template>
   <main>
-    <div>User {{ $route.params.name }}</div>
+    <!-- <div>User {{ $route.params.name }}</div> -->
 
     <div v-if="fetchError">Oops! Error encountered: {{ fetchError.message }}</div>
     <div v-else-if="fetchData">
-      <pre>{{ fetchData }}</pre>
-      <ul v-for="data in fetchData.data" :key="data.id">
-        <li>{{ data.attributes.name }}</li>
-        <li>{{ data.attributes.idStartGG }}</li>
-      </ul>
+      <!-- <pre>{{ fetchData }}</pre> -->
+      <div v-for="data in fetchData.data" :key="data.id">
+        <p>{{ data.attributes.name }}</p>
+        <p>{{ data.attributes.idStartGG }}</p>
+        <Charts :player-slug="data.attributes.idStartGG" />
+        <!-- <Charts player-slug="300903d2" /> -->
+      </div>
     </div>
     <div v-else>Loading...</div>
 
-    <Charts player-slug="300903d2" />
   </main>
 </template>
 
 <script>
 import { useFetch } from '../utils/fetch.js'
 import Charts from '../components/Charts.vue'
+import {watch} from 'vue'
+
 export default {
   name: 'PlayerView',
   props: {
@@ -27,15 +30,30 @@ export default {
   data () {
       return {
           players: [],
-          // idStartGG: '300903d2',
+          // idStartGG: '',
           // idStartGG: route.params.id
       }
   },
   setup () {
-    const { fetchData, fetchError } = useFetch(`http://localhost:1337/api/players?populate=*&filters[idStartGG][$eq]=d5ce37b2`)
+    // const { fetchData, fetchError } = useFetch(`http://localhost:1337/api/players?populate=*&filters[name][$eq]=${this.route.params.name}`)
+    const { fetchData, fetchError } = useFetch(`http://localhost:1337/api/players?populate=*&filters[name][$eq]=Ganondeurf`)
 
-    console.log(fetchData);
     // fetchData.data.map((dat) => console.log(dat.attributes.name))
+    
+    watch(fetchData, () => {
+      console.log(fetchData.value.meta);
+    })
+//     watch(fetchData, (props) => {
+//       /* triggers on deep mutation to state */
+      
+//       // console.log(this.idStartGG);
+      
+//       // console.log(fetchData.value.data[0].attributes.idStartGG);
+//       fetchData.value.data.map((item) => {
+//       props.idStart = '300903d2'
+//       console.log(item.attributes.name);
+//     })
+// })
     return {
       fetchData,
       fetchError,
