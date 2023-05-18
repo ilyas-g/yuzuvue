3da66d97d5267350c7992b088ff43da1273f6038
ilyas-g <ilyas15069i@gmail.com>
2023-05-18 16:58:08 +0200

    Intégration de la homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,30 @@
 <template>
-    <main>
-        <h1>Home</h1>
-            <div v-if="error">
-      {{ error }}
-    </div>
-    <ul v-else>
-      <li v-for="restaurant in restaurants" :key="restaurant.id">
-        {{ restaurant.attributes.name }}
-      </li>
-    </ul>
+    <main class="homepage">
+      <div>
+        <h1><span class="d-block yellow">Yuzu</span>Gaming</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam dolores tempore, pariatur quas reiciendis praesentium.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eum provident voluptate. 
-            Voluptatum eius consectetur voluptate quis voluptas ipsam, et in distinctio ipsum perspiciatis saepe modi dolorum officiis eum, 
-            sint, sapiente est nemo amet cupiditate?
-        </p>
+        <p class="text-uppercase yellow">#Yuzurenext</p>
+      </div>
+
+      <img :src="lgoo" alt="Yuzu Gaming" width="450" />
+      <!-- <div v-if="error">
+        {{ error }}
+      </div>
+      <ul v-else>
+        <li v-for="restaurant in restaurants" :key="restaurant.id">
+          {{ restaurant.attributes.name }}
+        </li>
+      </ul>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eum provident voluptate. 
+          Voluptatum eius consectetur voluptate quis voluptas ipsam, et in distinctio ipsum perspiciatis saepe modi dolorum officiis eum, 
+          sint, sapiente est nemo amet cupiditate?
+      </p> -->
     </main>
 </template>
 
 <script>
 import axios from 'axios'
-
+import lgoo from "@/assets/yuzu_logoo.svg";
 export default {
   name: 'HomePage',
   data () {
@@ -31,7 +36,7 @@ export default {
     setup () {
 
     return {
-
+      lgoo,
       myObject: {
         title: 'How to do lists in Vue',
         author: 'Jane Doe',
@@ -49,3 +54,37 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.homepage {
+  h1 {
+    line-height: 1;
+    text-transform: uppercase;
+    // text-align: center;
+    @media screen and (min-width: $breakpoint-md) {
+      font-size: 5em;
+    }
+    @media screen and (min-width: $breakpoint-lg) {
+      font-size: 4em;
+    }
+  }
+
+  p {
+    margin: 1em 0;
+  }
+  @media screen and (min-width: $breakpoint-lg) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5em;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  @media screen and (min-width: $breakpoint-lg) {
+    width: 80%;
+  }
+}
+</style>
